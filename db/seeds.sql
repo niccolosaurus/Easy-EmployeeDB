3c3a79463b6aead5af2fcb58f1a6c96ae5086553
Niccolo Eck <nicco.eck@gmail.com>
2022-03-07 16:44:42 -0800

    First Commit, db folder containing my Schema and Seeds

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department(name)
+VALUES  ("Engineering"),
+        ("Finance"),
+        ("Legal"),
+        ("Sales");
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Lead Engineer", 150000, 1),
+        ("Software Engineer", 120000, 1),  
+        ("Account Manager", 160000, 2),
+        ("Accountant", 125000, 2),
+        ("Legal Team Lead", 250000, 3),
+        ("Lawyer", 190000, 3),
+        ("Sales Lead", 100000, 4),
+        ("Salesperson", 80000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Niccolo", "Eck", 1, null),
+        ("Abigail", "Eck", 2, 1),
+        ("Kassie", "Pascual", 3, null),
+        ("Rodelle", "Sacman", 4, 3),
+        ("Orion", "Wu", 5, null),
+        ("Ronell", "Santos", 6, 5),
+        ("Jim", "Del Carmen", 7, null),
+        ("Katie", "Paul", 8, 7);
